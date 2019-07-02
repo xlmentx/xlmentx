a928f434f87eec34e49d9a6e2c54b00110c10b0f
Ari Cortes <Ari Cortes@CORSAIRONE.home>
2019-07-01 17:11:00 -0700

    work on textures

--- games/racing/Track.java
+++ games/racing/Track.java
@@ -30,8 +30,10 @@ import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Paint;
+import javafx.scene.paint.RadialGradient;
 import javafx.scene.paint.Stop;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class Track
@@ -53,13 +55,16 @@ public class Track
 							pDeclineRates =	{0.0,   0.0,   	0.0}; 
 					
 	private static Group 	background,  midground, foreground, platforms;
-	private static double 	bDistance = 300,
-							mDistance = 200,
-							fDistance = 100;
+	private static double 	bDistance = 2400,
+							mDistance = 1200,
+							fDistance = 400;
+							//pDistance = 1;
 	
-	private static Color 	sColor = Color.DEEPSKYBLUE,
-							gColor = Color.SANDYBROWN,
-							fColor = Color.WHITE;
+	private static Color 	lColor = Color.LIGHTGOLDENRODYELLOW,
+							sColor = Color.DEEPSKYBLUE,
+							fColor = Color.WHITE,
+							mColor = Color.FORESTGREEN,
+							gColor = Color.LIGHTGOLDENRODYELLOW;
 	private static Image 	rocks = new Image("image/scenery/Rocks.png");
 	
 	
@@ -70,55 +75,51 @@ public class Track
 	static void newTrack(int tLength)
 	{	// Background
 		background = new Group();
-		Polygon  sky = newPolygon(new double[2], Resolution, 0, sColor);
+		Rectangle  sky = new Rectangle(Resolution[0], Resolution[1], sColor);
 		double[] mPosition = {0, Resolution[1]*0.5},
 				 mDimension = {Resolution[0], Resolution[1]*0.3};
-		Paint 	 mTexture = blend(rocks, sColor, 0.5);
-		Polygon  bMountain = newMountain(mPosition, mDimension, mTexture);
+		Polygon  bMountain = newMountain(mPosition, mDimension, bDistance);
 		bMountain.setEffect(new DropShadow(127, fColor));
 		background.getChildren().addAll(sky, bMountain);
-		background.setCache(true);
-		background.setCacheHint(CacheHint.QUALITY);
 		
 		// Midground
 		midground = new Group();
-		mPosition = new double[] {-mDimension[0]/2, Resolution[1]*0.6};
-		mTexture = blend(rocks, sColor, mDistance/bDistance*0.5);
+		mPosition = new double[] {-mDimension[0]*0.2, Resolution[1]*0.6};
+		mDimension = new double[] {Resolution[0], Resolution[1]*0.3};
 		for(int i = 0; i < tLength/mDistance; i++)
-		{	Polygon mMountain = newMountain(mPosition, mDimension, mTexture);
+		{	Polygon mMountain = newMountain(mPosition, mDimension, mDistance);
 			midground.getChildren().add(mMountain);
 		}
 		midground.setEffect(new DropShadow(127, fColor));
 		
 		// Foreground
 		foreground = new Group();
-		mPosition = new double[] {-mDimension[0]/5, Resolution[1]*0.7};
-		mTexture = blend(rocks, sColor, fDistance/bDistance*0.5);
+		mPosition = new double[] {-mDimension[0]*0.5, Resolution[1]*0.7};
+		mDimension = new double[] {Resolution[0], Resolution[1]*0.3};
 		for(int i = 0; i < tLength/fDistance; i++)
-		{	Polygon fMountain = newMountain(mPosition, mDimension, mTexture);
+		{	Polygon fMountain = newMountain(mPosition, mDimension, fDistance);
 			foreground.getChildren().add(fMountain);
 		}
 		foreground.setEffect(new DropShadow(127, fColor));
-
+		
 		// Platforms
 		platforms = new Group();
-		double[] pPosition = {-Resolution[0], Resolution[1]*0.8};
-		Paint 	 pTexture = blend(rocks, sColor, 0);
-		while(pPosition[0] < Resolution[0])
-		{	double[] pDimension = {random(xRange, pWidthRates), Resolution[0]};
-			platforms.getChildren().add(newPolygon(pPosition, pDimension, 0, pTexture));
-		}
+		double[] pPosition = {-Resolution[0]/2, Resolution[1]*0.8},
+				 pDimension = {Resolution[0]*2, Resolution[1]};
+		Polygon runWay = newPolygon(pPosition, pDimension, 0);
+		platforms.getChildren().add(runWay);
+		
 		while(pPosition[0] < tLength)
 		{	pPosition[0] += random(xRange, pGapRates, 0);
 			pPosition[1] += random(yRange, pDropRates, -random(yRange, pWallRates, 0));
-			double[] pDimension = {random(xRange, pWidthRates, xRange[0]), Resolution[0]};
+			pDimension = new double[] {random(xRange, pWidthRates, xRange[0]), Resolution[0]};
 			double slope = random(sRange, pDeclineRates, -random(sRange, pInclineRates, 0));
-			platforms.getChildren().add(newPolygon(pPosition, pDimension, slope, pTexture));
+			platforms.getChildren().add(newPolygon(pPosition, pDimension, slope));
 		}
 	}
 	
 	// Create Mountain
-	private static Polygon newMountain(double[] position,  double[] dimension, Paint fill)
+	private static Polygon newMountain(double[] position, double[] dimension, double distance)
 	{	double[] 	wRange = {0, dimension[0]*0.08},
 				 	sRange = {0, dimension[1]/(dimension[0]*0.4)},
 				 	sRates = {1, 0, 0, 0.8};
@@ -127,7 +128,7 @@ public class Track
 		double[] start = position.clone();		
 		while(position[0]+wRange[1]*2 < start[0]+dimension[0] && position[1] <= start[1])
 		{	double width = random(wRange),
-				   slope = random(sRange, sRates)*Math.signum(position[0]-start[0]-dimension[0]/2);
+			   	   slope = random(sRange, sRates)*Math.signum(position[0]-start[0]-dimension[0]/2);
 			if(position[0] >= start[0]+dimension[0]*0.4 && slope < 0)
 			{	slope /= 3;
 			}
@@ -136,12 +137,15 @@ public class Track
 		mountain.getPoints().addAll(position[0] = start[0]+dimension[0], position[1] = start[1]);
 		mountain.getPoints().addAll(position[0], Resolution[1]);
 		
-		mountain.setFill(fill);
+		Stop highlight = new Stop(0, blend(mColor.saturate(), sColor, distance/bDistance*0.5)),
+			 shadow = new Stop(1, blend(Color.BLACK, sColor, distance/bDistance*0.5));
+		mountain.setFill( new RadialGradient(-90, 1, 0.5, 0.9, 1, true, null, highlight, shadow));
+		
 		return mountain;
 	}
 		
 	// Create Polygon
-	private static Polygon newPolygon(double[] position, double[] dimension, double slope, Paint fill)
+	private static Polygon newPolygon(double[] position, double[] dimension, double slope)
 	{	Polygon p = new Polygon
 		(	(int)position[0], 			position[1],
 			position[0]+dimension[0], 	position[1]+dimension[0]*slope,
@@ -150,7 +154,8 @@ public class Track
 		);
 		position[0] += dimension[0];
 		position[1] += dimension[0]*slope;
-		p.setFill(fill);
+		
+		p.setFill(gColor);
 		return p;
 	}
 	
