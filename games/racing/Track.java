a5c4e3f962b015de721f73439f4d02bf9481c0bf
Ari Cortes <Ari Cortes@CORSAIRONE.home>
2019-08-06 18:13:58 -0700

    g

--- games/racing/Track.java
+++ games/racing/Track.java
@@ -72,7 +72,7 @@ public class Track
 	static void newTrack(int tLength)
 	{	// new Background
 		background = new Group();	
-		double 	fog = 0;					
+		double 	fog = 1;					
 		Stop[]	sColors = new Stop[10]; 
 		sColors[0] = new Stop(0.5*fog, sColor);
 		for(int i = 0; i < sColors.length-1; i++)
@@ -81,8 +81,8 @@ public class Track
 			sColors[i+1] = new Stop(y, 
 					
 					blend(blend(blend(mColor, Color.BLACK, 									z
-							), sColor , 													z*z	
-							), fColor, 														z
+							), sColor , 													z
+							), fColor, 														z*z
 							)
 									
 					);			
@@ -115,26 +115,26 @@ public class Track
 			}	
 			
 			//	Color	summit = blend(blend(mColor, Color.BLACK, z), sColor, z*z),
-			
+			//	Fog != z(too strong)
 			//	Black 		z
-				//	Sky		z										Fog:	z	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
-				//			z*z										Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
-				//			1.82-Math.sqrt(4-Math.pow(z+0.82,2))	Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//	Sky		z										Fog:		z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//			z*z										Fog:		z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//			1.82-Math.sqrt(4-Math.pow(z+0.82,2))	Fog:		z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
 			
 			//	Black 		z*z
 				//	Sky		z										Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
-				//			z*z										Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
-				//			1.82-Math.sqrt(4-Math.pow(z+0.82,2))	Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//			z*z										Fog:		z*z		1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//			1.82-Math.sqrt(4-Math.pow(z+0.82,2))	Fog:		z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
 		
 			//	Black 		1.82-Math.sqrt(4-Math.pow(z+0.82,2))
-				//	Sky		z										Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
-				//			z*z										Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
-				//			1.82-Math.sqrt(4-Math.pow(z+0.82,2))	Fog:	z,	z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//	Sky		z										Fog:		z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//			z*z										Fog:		z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
+				//			1.82-Math.sqrt(4-Math.pow(z+0.82,2))	Fog:		z*z,	1.826-Math.sqrt(4-Math.pow(z+0.82,2))
 				
 			double 	z = (1-(i+1)/(layers+1));
 System.out.println("z2:"+z);			
-			Color	summit = blend(blend(mColor, Color.BLACK, 								z
-					), sColor , 															z*z	
+			Color	summit = blend(blend(mColor, Color.BLACK, 								z*z
+					), sColor , 															z
 					),
 					base = blend(summit, fColor, 											z
 					)
@@ -183,7 +183,8 @@ System.out.println("z2:"+z);
 	}
 	
 	private static Color blend(Color a, Color b, double ratio)
-	{	return Color.rgb
+	{	System.out.println(ratio);
+		return Color.rgb
 		(	(int)(255*(a.getRed()+(b.getRed()-a.getRed())*ratio)), 
 			(int)(255*(a.getGreen()+(b.getGreen()-a.getGreen())*ratio)), 
 			(int)(255*(a.getBlue()+(b.getBlue()-a.getBlue())*ratio))
